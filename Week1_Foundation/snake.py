6e04e048fd86ede371fde27ea921ef147dc5bd7d
Chhetrimandip <chhetrimandip91@gmail.com>
2026-01-07 23:34:53 +0545

    1/2

---
+++ Week1_Foundation/snake.py
@@ -0,0 +1,97 @@
+import turtle
+import time
+import random
+
+# Game Setup
+delay = 0.1
+score = 0
+high_score = 0
+
+# Screen
+wn = turtle.Screen()
+wn.title("Snake Game by Python")
+wn.bgcolor("black")
+wn.setup(width=600, height=600)
+wn.tracer(0)
+
+# Snake Head
+head = turtle.Turtle()
+head.speed(0)
+head.shape("square")
+head.color("white")
+head.penup()
+head.goto(0,0)
+head.direction = "stop"
+
+# Snake Food
+food = turtle.Turtle()
+food.speed(0)
+food.shape("circle")
+food.color("red")
+food.penup()
+food.goto(0,100)
+
+segments = []
+
+# Functions
+def go_up():
+    if head.direction != "down": head.direction = "up"
+def go_down():
+    if head.direction != "up": head.direction = "down"
+def go_left():
+    if head.direction != "right": head.direction = "left"
+def go_right():
+    if head.direction != "left": head.direction = "right"
+
+def move():
+    if head.direction == "up":
+        y = head.ycor()
+        head.sety(y + 20)
+    if head.direction == "down":
+        y = head.ycor()
+        head.sety(y - 20)
+    if head.direction == "left":
+        x = head.xcor()
+        head.setx(x - 20)
+    if head.direction == "right":
+        x = head.xcor()
+        head.setx(x + 20)
+
+# Keyboard bindings
+wn.listen()
+wn.onkeypress(go_up, "w")
+wn.onkeypress(go_down, "s")
+wn.onkeypress(go_left, "a")
+wn.onkeypress(go_right, "d")
+
+# Main Game Loop
+while True:
+    wn.update()
+    
+    # Check for collision with food
+    if head.distance(food) < 20:
+        x = random.randint(-290, 290)
+        y = random.randint(-290, 290)
+        food.goto(x,y)
+        
+        # Add segment
+        new_segment = turtle.Turtle()
+        new_segment.speed(0)
+        new_segment.shape("square")
+        new_segment.color("grey")
+        new_segment.penup()
+        segments.append(new_segment)
+        
+    # Move the segments
+    for index in range(len(segments)-1, 0, -1):
+        x = segments[index-1].xcor()
+        y = segments[index-1].ycor()
+        segments[index].goto(x,y)
+        
+    if len(segments) > 0:
+        x = head.xcor()
+        y = head.ycor()
+        segments[0].goto(x,y)
+
+    move()
+    time.sleep(delay)
